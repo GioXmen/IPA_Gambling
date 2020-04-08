14b3295ec08c5971c3a80435b64cc82db0c0b18c
Ernestas <Ernestas.Lobacevskis@nortal.com>
2020-04-09 02:08:57 +0300

    Working version 1 - Add ability to bet, draw race/refresh screen, first working version

---
+++ GamblingGame/Horse.cs
@@ -0,0 +1,21 @@
+﻿namespace GamblingGame
+{
+    internal class Horse
+    {
+        public string id { get; set; }
+
+        public int LuckScore { get; set; }
+
+        public int RealScore { get; set; }
+
+        public Horse()
+        {
+        }
+
+        public Horse(string id, int LuckScore) { 
+            this.id = id;
+            this.LuckScore = LuckScore;
+            this.RealScore = 0;
+        }
+    }
+}
