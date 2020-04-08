5555b1c0c94dc9c079cf5012d60991d40414490d
Ernestas <Ernestas.Lobacevskis@nortal.com>
2020-04-09 00:39:44 +0300

    Complete ASCII art draw logic, join ascii art and formatting. Refactor

--- GamblingGame/Program.cs
+++ GamblingGame/Program.cs
@@ -11,29 +11,22 @@ namespace GamblingGame
         [DllImport("kernel32.dll", ExactSpelling = true)]
         private static extern IntPtr GetConsoleWindow();
         private static IntPtr ThisConsole = GetConsoleWindow();
+
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
         private const int HIDE = 0;
-        private const int MAXIMIZE = 3;
         private const int MINIMIZE = 6;
         private const int RESTORE = 9;
+        private const int MAXIMIZE = 3;
         static void Main(string[] args)
         {
             Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
             ShowWindow(ThisConsole, MAXIMIZE);
 
-            Console.WriteLine(new string('-', 474));
-            Console.WriteLine("");
-            Console.WriteLine(Art.horse(3));
-            Console.WriteLine(Art.horse(7));
-            Console.WriteLine(Art.horse(5));
-            Console.WriteLine(Art.horse(9));
-            Console.WriteLine("");
-            Console.WriteLine(new string('-', 474));
-
-            Console.WriteLine(Art.JoinLetters<Art.MyFont>(2, "dh"));
-
-
+            Draw.drawHorses(0, 0, 0, 0);
+            Draw.printMoney(500);
+            Draw.printMoneyWin(500);
+            Draw.printMoneyLoose(500);
         }
     }
 }
